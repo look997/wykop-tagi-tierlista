307420712adb0541159d9e04fce67f510778415a
Łukasz Polowczyk <dev.lukaszpolowczyk@gmail.com>
2022-06-20 23:13:41 +0200

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,25 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-node';
+// import Inspector from 'vite-plugin-svelte-inspector';
 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'public',
-      assets: 'public',
-      fallback: null,
-      precompress: false
-    }),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+    vite: {
+      // plugins: [Inspector()],
+    },
+		// adapter: adapter()
+		adapter: adapter(),
     
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true
+    },
+		browser: {
+      router: false
     }
-  }
+	}
 };
+
+export default config;
